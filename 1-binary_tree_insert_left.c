6515d625e3ecbbe7079d455a69f1a4c86f50b0fc
Omar-algassim <omarggg2@gmail.com>
2024-02-28 01:44:34 +0200

    update

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,11 +15,18 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	new = malloc(sizeof(binary_tree_t));
+	new = binary_tree_node(parent, value);
 	if (new == NULL)
 		return (NULL);
 
-	new->n = value;
+	if (parent->left == NULL)
+		parent->left = new;
+	else
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
+		parent->left = new;
+	}
 	return (new);
 }
 
