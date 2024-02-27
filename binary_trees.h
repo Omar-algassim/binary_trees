a277794de875e7ee67fc76e25b0da237c5797efe
Omar-algassim <omarggg2@gmail.com>
2024-02-28 00:14:07 +0200

    upload all files empty for the mandotry

--- binary_trees.h
+++ binary_trees.h
@@ -5,13 +5,13 @@
 #include <stdlib.h>
 
 /**
- *  * struct binary_tree_s - Binary tree node
- *   *
- *    * @n: Integer stored in the node
- *     * @parent: Pointer to the parent node
- *      * @left: Pointer to the left child node
- *       * @right: Pointer to the right child node
- *        */
+* struct binary_tree_s - Binary tree node
+*
+* @n: Integer stored in the node
+* @parent: Pointer to the parent node
+* @left: Pointer to the left child node
+* @right: Pointer to the right child node
+ */
 struct binary_tree_s
 {
 	    int n;
@@ -41,6 +41,6 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-
+void binary_tree_print(const binary_tree_t *);
 
 #endif
