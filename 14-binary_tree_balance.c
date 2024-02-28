2f0cf48df7512dc18f0061d890aa70e7b368076d
b7sh <mohammedabdelbasit500@gmail.com>
2024-02-28 12:37:38 -0800

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,10 +1,41 @@
 #include "binary_trees.h"
 
-/***/
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: ter to the root node of the tree to measure the balance factor
+ * Return: If tree is NULL, return 0
+ * otherwise, return the balance
+ */
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	return 0;
+	if (tree != NULL)
+	{
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	}
+	return (0);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: the pointer root of tree
+ * Return: the height of the tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t right;
+	size_t left;
+
 	if (tree == NULL)
-		return 0;
+		return (0);
+	if (tree->right == NULL && tree->left == NULL)
+		return (0);
+
+	right = binary_tree_height(tree->right);
+	left = binary_tree_height(tree->left);
+	if (right > left)
+		return (right + 1);
+	else
+		return (left + 1);
 }
