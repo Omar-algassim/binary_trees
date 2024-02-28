70169855d4766b071c7455f36c312ec7986d7bc2
b7sh <mohammedabdelbasit500@gmail.com>
2024-02-27 17:49:52 -0800

    update

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,9 +10,9 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t depth;
+	size_t depth = 0;
 
-	if (tree == NULL)
+	if (tree->parent == NULL)
 		return (0);
 
 	depth = 1 + binary_tree_depth(tree->parent);
