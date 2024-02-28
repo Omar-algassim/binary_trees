5f6c2661449810f349ee1f4ee2dd894cc2572aa7
b7sh <mohammedabdelbasit500@gmail.com>
2024-02-28 12:40:53 -0800

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -29,8 +29,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->right == NULL && tree->left == NULL)
-		return (0);
 
 	right = binary_tree_height(tree->right);
 	left = binary_tree_height(tree->left);
