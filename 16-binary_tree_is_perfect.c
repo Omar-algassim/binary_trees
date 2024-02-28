34e3fab922cd09f10a319d77fae530bb10417921
b7sh <mohammedabdelbasit500@gmail.com>
2024-02-28 13:12:16 -0800

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -17,13 +17,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
-	
-	binary_tree_is_perfect(tree->left);
-	binary_tree_is_perfect(tree->right);
 
 	if (left == right)
 		return (1);
-	return (0);
+	else
+		return (0);
 }
 
 /**
