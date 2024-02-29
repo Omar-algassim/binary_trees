3bf6d82ef9695d9ec75bb174856dbae3a409e44b
b7sh <mohammedabdelbasit500@gmail.com>
2024-02-29 10:19:05 -0800

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -50,3 +50,28 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	else
 		return (right + 1);
 }
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: the pointer of root tree
+ * Return: 0 if the tree is full
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int right, left;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->right == NULL && tree->left == NULL)
+		return (1);
+	if ((tree->right != NULL && tree->left == NULL)
+			|| (tree->right == NULL && tree->left != NULL))
+		return (0);
+
+	right = binary_tree_is_full(tree->right);
+	left = binary_tree_is_full(tree->left);
+
+	if (right == 1 && left == 1)
+		return (1);
+	return (0);
+}
